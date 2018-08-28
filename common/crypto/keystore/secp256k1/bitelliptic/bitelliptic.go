618e309ba30c650e32c0c6d5cca0545fff7e747b
tsorf <afrostyz@163.com>
2018-08-28 18:58:13 +0800

    Update license

--- common/crypto/keystore/secp256k1/bitelliptic/bitelliptic.go
+++ common/crypto/keystore/secp256k1/bitelliptic/bitelliptic.go
@@ -1,6 +1,3 @@
-// Copyright 2010 The Go Authors. All rights reserved.
-// Copyright 2011 ThePiachu. All rights reserved.
-//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
