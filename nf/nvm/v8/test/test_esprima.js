47fe9726f933cc65a858bb1e7477496d9d551d72
tsorf <afrostyz@163.com>
2018-09-23 21:47:12 +0800

    Deal with license in nvm

--- nf/nvm/v8/test/test_esprima.js
+++ nf/nvm/v8/test/test_esprima.js
@@ -1,21 +1,3 @@
-// Copyright (C) 2017 go-nebulas authors
-//
-// This file is part of the go-nebulas library.
-//
-// the go-nebulas library is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// the go-nebulas library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with the go-nebulas library.  If not, see <http://www.gnu.org/licenses/>.
-//
-
 var esprima = require('../lib/esprima.js');
 
 var source = "'use strict';\nvar SampleContract = function() {\n    LocalContractStorage.defineProperties(this, {\n        name: null,\n        count: null\n    });\n    LocalContractStorage.defineMapProperty(this, \"allocation\" + 123);\n    this.a = 0;\n};";
