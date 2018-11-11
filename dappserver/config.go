fd21f445dccd6cf1b6470191460d5db1bb65bd86
tsorf <afrostyz@163.com>
2018-11-11 17:50:40 +0800

    Add dappserver config and conf file

---
+++ dappserver/config.go
@@ -0,0 +1,32 @@
+package dappserver
+
+import (
+	"io/ioutil"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/gogo/protobuf/proto"
+	"github.com/pepperdb/pepperdb-core/common/util/logging"
+	"github.com/pepperdb/pepperdb-core/dappserver/pb"
+)
+
+// LoadConfig loads configuration from file.
+func LoadConfig(file string) *dappserverpb.Config {
+	var conf string
+	b, err := ioutil.ReadFile(file)
+	if err != nil {
+		logging.CLog().WithFields(logrus.Fields{
+			"err": err,
+		}).Fatal("Failed to read the config file: %s.", file)
+	}
+
+	conf = string(b)
+
+	pb := new(dappserverpb.Config)
+	if err := proto.UnmarshalText(conf, pb); err != nil {
+		logging.CLog().WithFields(logrus.Fields{
+			"err": err,
+		}).Fatal("Failed to parse the config file: %s.", file)
+	}
+	return pb
+}
